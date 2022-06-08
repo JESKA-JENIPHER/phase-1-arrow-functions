59eaf5a3ec50fd24b836395c55ea554fb4fbf07e
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-06-08 09:10:43 +0300

    add functions

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1) has a function expression called divide
+const divider = (a, b) => a / b;
+// console.log(divider(2000, 100));
+
+// 2) divide divides 2000 by 100
+const divide = () => 2000 / 100;
+
+// 3) has an arrow function called square
+const square = (x) => x * x;
+
+// 4) square arrow function takes one parameter and multiplies it times itself
+// 5) has an arrow function called add
+// 6) add arrow function takes two parameters and adds them together
+const add = (a, b) => a + b;
